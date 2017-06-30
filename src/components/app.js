28ad031a6ea4ab37f074080c7fdd667efb0bd652
liuderchi <liuderchi@gmail.com>
2017-06-30 22:56:15 +0800

    use functional component in app.js
    - to enhance efficiency

--- src/components/app.js
+++ src/components/app.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import SearchBar from '../containers/search_bar.js';
 import WeatherList from '../containers/weather_list.js';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Weather Helper</h1>
-        <SearchBar />
-        <WeatherList />
-      </div>
-    );
-  }
+export default () => {
+  return (
+    <div>
+      <h1>Weather Helper</h1>
+      <SearchBar />
+      <WeatherList />
+    </div>
+  );
 }
