914bdfac742d98e15b3dc960624641263d2c358d
liuderchi <liuderchi@gmail.com>
2017-07-13 10:14:26 +0800

    add recompose to weather_list.js (1/2)
    - remove es6 class, add setPropTypes

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,53 +1,49 @@
-import React, { Component } from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
 import GoogleMap from '../components/google_map';
+import { compose, pure, setPropTypes } from 'recompose';
 
-class WeatherList extends Component {
-  constructor(props) {
-    super(props);
+const renderWeather = (cityData) => {    // render weather for each city
+  const { name, coord: { lat, lon } } = cityData.city;
+  const temps = cityData.list.map(weather => weather.main.temp);
+  const pressures = cityData.list.map(weather => weather.main.pressure);
+  const humidities = cityData.list.map(weather => weather.main.humidity);
 
-  }
-
-  renderWeather(cityData) {    // render weather for each city
-    const { name, coord } = cityData.city;
-    const temps = cityData.list.map(weather => { return weather.main.temp; });
-    const pressures = cityData.list.map(weather => { return weather.main.pressure; });
-    const humidities = cityData.list.map(weather => { return weather.main.humidity; });
-
-    return (
-      <tr key={name}>
-        <td><GoogleMap lat={coord.lat} lon={coord.lon} /></td>
-        <td><Chart data={temps} units="K" color="green"/></td>
-        <td><Chart data={pressures} units="hPa" color="blue"/></td>
-        <td><Chart data={humidities} units="%" color="brown"/></td>
-      </tr>
-    )
-  }
-
-  render() {
-
-    return (
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>City</th>
-            <th>Temperature (K)</th>
-            <th>Pressure (hPa)</th>
-            <th>Humidity (%)</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.weather.map(this.renderWeather)}
-        </tbody>
-      </table>
-    )
-  }
+  return (
+    <tr key={name}>
+      <td><GoogleMap lat={lat} lon={lon} /></td>
+      <td><Chart data={temps} units="K" color="green"/></td>
+      <td><Chart data={pressures} units="hPa" color="blue"/></td>
+      <td><Chart data={humidities} units="%" color="brown"/></td>
+    </tr>
+  )
 }
+
+const enhance = compose(
+  pure,
+  setPropTypes({ weather: PropTypes.array })
+)
+
+const WeatherList = ({ weather }) => (
+  <table className="table table-hover">
+    <thead>
+      <tr>
+        <th>City</th>
+        <th>Temperature (K)</th>
+        <th>Pressure (hPa)</th>
+        <th>Humidity (%)</th>
+      </tr>
+    </thead>
+    <tbody>
+      {weather.map(renderWeather)}
+    </tbody>
+  </table>
+)
 
 
 function mapStateToProps({ weather }) {
   return { weather };   // { weather } === { weather: weather }
 }
 
-export default connect(mapStateToProps)(WeatherList);
+export default connect(mapStateToProps)(enhance(WeatherList));
