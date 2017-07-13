2433aeb498d3c7e461ed3d244b54cd76de76b8c9
liuderchi <liuderchi@gmail.com>
2017-07-13 10:16:38 +0800

    apply recompose to CityWeather
    - pure, mapProps, setPropTypes

--- src/components/city_weather.js
+++ src/components/city_weather.js
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import Chart from '../components/chart'
 import GoogleMap from '../components/google_map'
+import { compose, pure, mapProps, setPropTypes } from 'recompose'
 
-const CityWeather = ({ cityWeather }) => {
-  const { lat, lon } = cityWeather.city.coord
-  const temps = cityWeather.list.map(weather => weather.main.temp)
-  const pressures = cityWeather.list.map(weather => weather.main.pressure)
-  const humidities = cityWeather.list.map(weather => weather.main.humidity)
+const enhance = compose(
+  pure,
+  mapProps((props) => {
+    const { cityWeather } = props
+    const { coord: { lat, lon} } = cityWeather.city
+    return {
+      ...props,
+      lat,
+      lon,
+      temps: cityWeather.list.map(weather => weather.main.temp),
+      pressures: cityWeather.list.map(weather => weather.main.pressure),
+      humidities: cityWeather.list.map(weather => weather.main.humidity),
+    }
+  }),
+  setPropTypes({
+    lat: PropTypes.number,
+    lon: PropTypes.number,
+    temps: PropTypes.arrayOf(PropTypes.number),
+    pressures: PropTypes.arrayOf(PropTypes.number),
+    humidities: PropTypes.arrayOf(PropTypes.number),
+  })
+)
 
-  return (
-    <tr>
-      <td><GoogleMap lat={lat} lon={lon} /></td>
-      <td><Chart data={temps} units="K" color="green"/></td>
-      <td><Chart data={pressures} units="hPa" color="blue"/></td>
-      <td><Chart data={humidities} units="%" color="brown"/></td>
-    </tr>
-  )
-}
+const CityWeather = ({ lat, lon, temps, pressures, humidities }) => (
+  <tr>
+    <td><GoogleMap lat={lat} lon={lon} /></td>
+    <td><Chart data={temps} units="K" color="green"/></td>
+    <td><Chart data={pressures} units="hPa" color="blue"/></td>
+    <td><Chart data={humidities} units="%" color="brown"/></td>
+  </tr>
+)
 
-export default CityWeather
+export default enhance(CityWeather)
