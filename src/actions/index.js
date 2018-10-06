7ac6845479d9083e4fdb7efc000ddb7ab60241e3
liuderchi <liuderchi@gmail.com>
2018-10-06 09:49:25 +0800

    fix(action): remove country code in query string, add CORS anywhere

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,15 @@
 import axios from 'axios'
 
+const CORS_ANYWHERE_DOMAIN = 'https://cors-anywhere.herokuapp.com'
 const API_KEY = '8da17980896fadeb0d58e0b0621f64b7'
-const COUNTRY_CODE = 'TW'
-const ROOT_UTL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+const OPEN_WEATHER_MAP_API = 'http://api.openweathermap.org/data/2.5/forecast'
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 // use variable to store action type for better maintainability
 
 export function fetchWeather(city) {
-  const url = `${ROOT_UTL}&q=${city},${COUNTRY_CODE}`
+  const url = `${CORS_ANYWHERE_DOMAIN}/${OPEN_WEATHER_MAP_API}` +
+    `?q=${city}&appid=${API_KEY}`
   let request = axios.get(url)
   // NOTE request is a promise
 
